af623c8e85b4bceee127c2a3a637ac612df7da81
cmhyett <criston.hyett@juliahub.com>
2026-05-31 19:00:34 -0500

    out with the old

--- LDM/__init__.py
+++
@@ -1,5 +0,0 @@
-__path__ = "LDM/src/"
-__all__ = ["LDM_TBNN",
-           "TBNN",
-           "utils",
-           "plotting_utils"]
--- LDM/src/old_code/types/DataDescription.jl
+++
@@ -1,8 +0,0 @@
-struct DataDescription
-    L::Float64;
-    dt::Float64;
-    Re::Float64;
-    NX::Float64;
-    NY::Float64;
-    NZ::Float64;
-end
